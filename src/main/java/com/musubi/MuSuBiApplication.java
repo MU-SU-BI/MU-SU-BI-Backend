f3d6812bdd828f07983d717c8d6a553ec4d73568
kimnoca <kimnoca@naver.com>
2024-07-19 16:51:59 +0900

    init : project init

---
+++ src/main/java/com/musubi/MuSuBiApplication.java
@@ -0,0 +1,13 @@
+package com.musubi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuSuBiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuSuBiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/musubi/MuSuBiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musubi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuSuBiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
