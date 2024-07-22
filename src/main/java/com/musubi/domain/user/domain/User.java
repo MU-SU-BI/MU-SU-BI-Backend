01f855a1259bafd89982d54cf99c6db349edadaa
kimnoca <kimnoca@naver.com>
2024-07-22 13:31:48 +0900

    chore : H2 DB User 예약어 지정해제

--- src/main/java/com/musubi/domain/user/domain/User.java
+++ src/main/java/com/musubi/domain/user/domain/User.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Table(name = "Users")
+//@Table(name = "Users")
 public class User extends BaseEntity {
 
     @Id
