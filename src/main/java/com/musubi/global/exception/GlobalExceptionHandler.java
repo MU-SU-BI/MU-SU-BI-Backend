5b88d939cb6a5804e868b409d788102997a4f56f
kimnoca <kimnoca@naver.com>
2024-10-05 22:29:09 +0900

    feat : RequestParam 누락 관련 예외처리

--- src/main/java/com/musubi/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/musubi/global/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.musubi.global.utils.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -31,5 +32,16 @@ public class GlobalExceptionHandler { //TODO : package 위치 이동 필요함
                 .build();
 
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(ex.getStatus()));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<?> missingServletRequestParameterException(MissingServletRequestParameterException ex) {
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+            .httpStatusCode(HttpStatus.BAD_REQUEST.value())
+            .errorMessage("요청 파라미터가 누적 되었습니다. : " + ex.getParameterName())
+            .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
