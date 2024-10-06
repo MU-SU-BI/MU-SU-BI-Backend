7fd7f31f56580669be32cf93ff27119930c42bba
kimnoca <kimnoca@naver.com>
2024-10-06 19:04:04 +0900

    fix : 지역 커뮤니티 SOS 알림 전송 기능 DTO 관련 오류 수정

--- src/main/java/com/musubi/domain/notification/dto/SOSRequestDto.java
+++ src/main/java/com/musubi/domain/notification/dto/SOSRequestDto.java
@@ -3,10 +3,12 @@ package com.musubi.domain.notification.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class SOSRequestDto {
-	private final Long guardianID;
+	private Long guardianID;
 }
