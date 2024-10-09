e700c8bef0933e8a50f6c811540ca17fd988c077
kimnoca <kimnoca@naver.com>
2024-10-09 18:30:06 +0900

    fix : 안전 구역 이탈 메시지 오타 수정

--- src/main/java/com/musubi/domain/notification/type/FCMValue.java
+++ src/main/java/com/musubi/domain/notification/type/FCMValue.java
@@ -9,7 +9,7 @@ public enum FCMValue {
 	EMERGENCY("긴급", "피 보호자가 응급 상황입니다."),
 	HELP("도움", "피 보호자가 도움이 필요한 상황입니다."),
 	SOS("SOS", "주변 커뮤니티에 도움이 필요한 사람이 있습니다."),
-	ALERT("알림", "피 보호자가 안전구역을 벗어 났습니ㅏ.");
+	ALERT("알림", "피 보호자가 안전구역을 이탈 했습니다.");
 
 	private final String title;
 	private final String body;
